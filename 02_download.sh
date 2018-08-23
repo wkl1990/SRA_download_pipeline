5bb2c5fcc20b64dcd273cd86ff6fc73379ca19d9
wkl1990 <dzq123456789@163.com>
2018-08-23 15:59:44 +0800

    fix bugs

--- 02_download.sh
+++ 02_download.sh
@@ -2,14 +2,15 @@
 echo path=\"${1}\"
 echo max-size=\"${2}\"
 echo input_krt=\"${3}\"
+echo sra_path=\"${4}\"
 # If downloading dbGaP data, prefetch must be called from within the project space folder
 cd $1
 # Download the SRA file
 prefetch.2.8.2 --max-size=$2 $3
 # delete temporary files 
-rm *.tmp.aspera-ckpt
-rm *.tmp.partial
-rm *.lock
-rm *.tmp
-rm *.vdbcache.cache
-rm *.vdbcache
+rm ${4}/*.tmp.aspera-ckpt
+rm ${4}/*.tmp.partial
+rm ${4}/*.lock
+rm ${4}/*.tmp
+rm ${4}/*.vdbcache.cache
+rm ${4}/*.vdbcache
